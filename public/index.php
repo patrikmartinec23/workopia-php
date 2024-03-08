36702b7d2122e22560d9eacba8bb2aa00834d160
Patrik <patrik.martinec1@gmail.com>
2024-03-08 20:42:29 +0100

    second commit

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
 require '../helpers.php';
+require basePath('Database.php');
+$config = require basePath('config/db.php');
 
-require basePath('views/home.view.php');
+$db = new Database($config);
+
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+$router->route($uri, $method);
